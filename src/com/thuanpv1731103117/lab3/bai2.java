3e3bdefd5cd0de634de3989dd3e65cb63ba8f2a8
vanthuan0306 <vanthuan0306@gmail.com>
2021-09-27 19:33:44 +0700

    bài 2 lab 3

---
+++ src/com/thuanpv1731103117/lab3/bai2.java
@@ -0,0 +1,15 @@
+package com.thuanpv1731103117.lab3;
+
+public class bai2 {
+    public static void main(String[] args) {
+        int x = 8;
+        for(int i=1;i<=9;i++){
+            System.out.printf("%d * %d = %d", x, i, x*i); 
+            System.out.println();
+            for(int j=1;j<=10;j++){
+                System.out.printf("%d * %d = %d", i, j, i*j);
+                System.out.println();
+            }
+        }
+    }
+}
