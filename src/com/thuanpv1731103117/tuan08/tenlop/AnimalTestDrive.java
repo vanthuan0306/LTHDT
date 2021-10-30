87287e7925a026ebff8d56021710e16d8c63267c
vanthuan0306 <vanthuan0306@gmail.com>
2021-10-30 17:43:04 +0700

    .

--- src/com/thuanpv1731103117/tuan08/tenlop/AnimalTestDrive.java
+++ src/com/thuanpv1731103117/tuan08/tenlop/AnimalTestDrive.java
@@ -3,13 +3,13 @@ package com.thuanpv1731103117.tuan08.tenlop;
 public class AnimalTestDrive {
     public static void main(String[] args) {
         //Kiểu cha
-        Animal animal;//khai báo biến tham chiếu/ Remote
+        //Animal animal;//khai báo biến tham chiếu/ Remote
 
 
         //Kiểu con
-        Wolf wolf = new Wolf();
+        //Wolf wolf = new Wolf();
 
-        animal = wolf;
+        //animal = wolf;
     }
 
 }
---
+++ src/com/thuanpv1731103117/tuan08/tenlop/WolfTestDrive.java
@@ -0,0 +1,8 @@
+package com.thuanpv1731103117.tuan08.tenlop;
+
+public class WolfTestDrive {
+    public static void main(String[] args) {
+        //Wolf wolf = new Wolf();
+        
+    }
+}
