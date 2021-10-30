f9288e1eeb5673320419c9ba2982434166bdce3f
vanthuan0306 <vanthuan0306@gmail.com>
2021-10-30 18:31:52 +0700

    bai1

--- src/com/thuanpv1731103117/tuan08/homework/SanPham.java
+++ src/com/thuanpv1731103117/tuan08/homework/SanPham.java
@@ -13,9 +13,21 @@ public class SanPham {
     protected void nhap (){
 
     }
+    public SanPham(){
+        tenSp = "Máy Tính Dell I5-9700F";
+        donGia = 7000000;
+        giamGia = 0;
+        thueNhapKhau = donGia*10/100;
+    }
 
     public double getThueNhapKhau() {
         return thueNhapKhau;
     }
     
+    protected void xuatThongTin(){
+        System.out.println("Tên sản phẩm: "+ tenSp);
+        System.out.println("Đơn giá sản phẩm: " + donGia);
+        System.out.println("Giá giảm: " +giamGia);
+        System.out.println("Thuế nhập khẩu: " +getThueNhapKhau());
+    }
 }
---
+++ src/com/thuanpv1731103117/tuan08/homework/SanPhamTestDrive.java
@@ -0,0 +1,9 @@
+package com.thuanpv1731103117.tuan08.homework;
+
+public class SanPhamTestDrive {
+    public static void main(String[] args) {
+        SanPham  sanPham = new SanPham();
+        sanPham.xuatThongTin();
+    }
+    
+}
