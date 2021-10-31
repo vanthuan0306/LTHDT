1c330b8eebfedd09284fd1e915fdd54bbe0d4e28
vanthuan0306 <vanthuan0306@gmail.com>
2021-10-31 14:41:20 +0700

    bai2

--- src/com/thuanpv1731103117/tuan08/homework/SanPham.java
+++ src/com/thuanpv1731103117/tuan08/homework/SanPham.java
@@ -1,11 +1,27 @@
 package com.thuanpv1731103117.tuan08.homework;
 
 public class SanPham {
+    protected int maSp;
     protected String tenSp;
     protected double donGia;
     protected double giamGia;
     protected double thueNhapKhau;
 
+    
+    public SanPham(){
+        // tenSp = "Máy Tính Dell I5-9700F";
+        // donGia = 7000000;
+        // giamGia = 0;
+        //thueNhapKhau = donGia*10/100;
+
+    }
+    public SanPham(int maSp,String tenSp, double donGia, double giamGia, double thueNhapKhau ){
+        this.maSp = maSp;
+        this.tenSp= tenSp;
+        this.donGia = donGia;
+        this.giamGia = giamGia; 
+    }
+    
     protected void xuat(){
 
     }
@@ -13,21 +29,50 @@ public class SanPham {
     protected void nhap (){
 
     }
-    public SanPham(){
-        tenSp = "Máy Tính Dell I5-9700F";
-        donGia = 7000000;
-        giamGia = 0;
-        thueNhapKhau = donGia*10/100;
+    
+    
+    
+    public int getMaSp() {
+        return maSp;
     }
 
-    public double getThueNhapKhau() {
+    public String getTenSp() {
+        return tenSp;
+    }
+    public double getDonGia() {
+        return donGia;
+    }
+    public double getGiamGia() {
+        return giamGia;
+    }
+
+    public void setMaSp(int maSp) {
+        this.maSp = maSp;
+    }
+    
+    public void setTenSp(String tenSp) {
+        this.tenSp = tenSp;
+    }
+    public void setDonGia(double donGia) {
+        this.donGia = donGia;
+    }
+    public void setGiamGia(double giamGia) {
+        this.giamGia = giamGia;
+    }
+    
+    private double getThueNhapKhau() {
+        thueNhapKhau = donGia*10/100;
         return thueNhapKhau;
     }
     
+
+    
     protected void xuatThongTin(){
-        System.out.println("Tên sản phẩm: "+ tenSp);
-        System.out.println("Đơn giá sản phẩm: " + donGia);
-        System.out.println("Giá giảm: " +giamGia);
+        System.out.println("Tên sản phẩm: "+ getTenSp());
+        System.out.println("Đơn giá sản phẩm: " + getDonGia());
+        System.out.println("Giá giảm: " +getGiamGia());
         System.out.println("Thuế nhập khẩu: " +getThueNhapKhau());
     }
+    
+
 }
--- src/com/thuanpv1731103117/tuan08/homework/SanPhamTestDrive.java
+++ src/com/thuanpv1731103117/tuan08/homework/SanPhamTestDrive.java
@@ -1,9 +1,69 @@
 package com.thuanpv1731103117.tuan08.homework;
 
+import java.util.Scanner;
+
 public class SanPhamTestDrive {
+    //cách 1
+    
     public static void main(String[] args) {
-        SanPham  sanPham = new SanPham();
-        sanPham.xuatThongTin();
+        
+        Scanner scanner = new Scanner(System.in);
+
+        SanPham sp1 = new SanPham();
+        System.out.print("Nhập mã sản phẩm 1: ");
+        sp1.setMaSp(scanner.nextInt());scanner.nextLine();
+        System.out.print("Nhập tên sản phẩm 1: ");
+        sp1.setTenSp(scanner.nextLine());
+        System.out.print("Đơn giá sản phẩm 1: ");
+        sp1.setDonGia(scanner.nextDouble());
+        System.out.print("Giá giảm: ");
+        sp1.setGiamGia(scanner.nextDouble());
+
+        SanPham sp2 = new SanPham();
+        System.out.print("Nhập mã sản phẩm 2: ");
+        sp2.setMaSp(scanner.nextInt());scanner.nextLine();
+        System.out.print("Nhập tên sản phẩm 2: ");
+        sp2.setTenSp(scanner.nextLine());
+        System.out.print("Đơn giá sản phẩm 2: ");
+        sp2.setDonGia(scanner.nextDouble());
+        System.out.print("Giá giảm: ");
+        sp2.setGiamGia(scanner.nextDouble());
+
+
+        System.out.println("---Danh sách sản phẩm---");
+        System.out.println("Sản phẩm 1");
+        sp1.xuatThongTin();
+        System.out.println("");
+        System.out.println("Sản phẩm 2");
+        sp2.xuatThongTin();
+        System.out.println("---End---");
     }
+    //c2
+    // public static void main(String[] args) {
+        
+    //     Scanner scanner = new Scanner(System.in);
+    //     System.out.print("Nhập số lượng sản phẩm: ");
+    //     int slsp= scanner.nextInt();
+    //     SanPham sp = new SanPham();
+
+    //     for(int i=1; i<=slsp; i++ ){
+    //         //SanPham sp = new SanPham();
+    //         System.out.print("Nhập mã sản phẩm " +i+ ": ");
+    //         sp.setMaSp(scanner.nextInt());scanner.nextLine();
+    //         System.out.print("Nhập tên sản phẩm " +i+ ": ");
+    //         sp.setTenSp(scanner.nextLine());
+    //         System.out.print("Đơn giá sản phẩm " +i+ ": ");
+    //         sp.setDonGia(scanner.nextDouble());
+    //         System.out.print("Giá giảm: ");
+    //         sp.setGiamGia(scanner.nextDouble());
+            
+            
+    //         System.out.println("Thông tin sản phẩm " +i+ ". ");
+    //         sp.xuatThongTin();
+    //         System.out.println("");
+    //     }
+        
+        
+    //}
     
 }
